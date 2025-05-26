3e116265efcd346061868851b63a90aebd59862f
Daan van Rooten <d.vanrooten@gmail.com>
2025-05-26 14:05:17 +0200

    iets
    adsasd

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <div class="Left_right_margin">
+        <div class="navbar">
+            <div class="menu">
+                <img src="images/menu.png" >
+            </div>
+            <div class="logo">
+                <img src="images/smokey_buns.png" >
+            </div>
+            <div class="talen">
+                <div class="wit">
+                <button>NL</button ></div>
+                <div class="zwart">
+                <button>EN</button ></div>
+            </div>
+            <div class="Inlog_Uitlog">
+                <button>Inloggen</button>
+            </div>
+            <div class="winkelmandje">
+                <img src="images/cart.svg" >
+            </div>
+        </div>
+
+
+    </div>
+</body>
+</html>
