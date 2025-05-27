309fc46a4fa46a856c520087c99e518feeb2c879
Daan van Rooten <d.vanrooten@gmail.com>
2025-05-27 13:50:28 +0200

    menu en contacten pagina
    ben wel vergeten om een aparte branche aan te maken

--- view/contact.php
+++ view/contact.php
@@ -12,5 +12,29 @@
 </head>
 <body>
     <?php echo $navbar; ?>
+    <div class="links_rechts_margin_contact">
+    <div class="Contacten_kop">
+        <p>Wil je met ons in contact?
+        Doe dat hier!</p>
+    </div>
+    <div class="contacten_grijze_rand">
+        <div class="naam">
+            <p>Naam</p>
+            <input type="text" placeholder="Vul hier je naam in">
+        </div>
+        <div class="email">
+            <p>Email</p>
+            <input type="email" placeholder="Vul hier je email in">
+        </div>
+        <div class="bericht">
+            <p>Bericht</p>
+            <textarea placeholder="Vul hier je bericht in"></textarea>
+        </div>
+        <div class="button_verstuur">
+            <button>Verstuur</button>
+    </div>
+    </div>
+    <div class="margin_bottom">
+        </div>
 </body>
 </html>
--- view/menu.php
+++ view/menu.php
@@ -1,8 +1,9 @@
 <?php
- include_once '../classes/navbar.php';
+include_once '../classes/navbar.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,7 +11,138 @@
     <link rel="stylesheet" href="../css/index.css">
     <script src="../js/index.js"></script>
 </head>
+
 <body>
     <?php echo $navbar; ?>
+    <div class="links_rechts_margin">
+        <div class="brugers_one_sale">
+            <p>Burgers on sale</p>
+        </div>
+        <div class="burger_menu">
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/Enkel_wopper.png">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Enkel wopper</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/Enkel_wopper.png">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Enkel wopper</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/Enkel_wopper.png">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Enkel wopper</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="drinken_menu">
+            <p>Drinken</p>
+        </div>
+        <div class="burger_menu">
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/coca-cola-original-330ml.webp">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Cola</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/fanta.png">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Fanta</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="sale_display">
+                <div class="enkel_wopper_img">
+                    <img src="../images/hero_cassis_blikjes_33cl_tray.jpeg">
+                </div>
+                <div class="textbox">
+                    <div class="text_margin">
+                        <div class="burger_font">
+                            <p>Cassis</p>
+                        </div>
+                        <div class="text_prijs_burger">
+                            <p>€ 2,50</p>
+                            <p>De enige echte wopper, met een rokerige smaak</p>
+                        </div>
+                        <div class="button_mandje">
+                            <button>Voeg toe aan je mandje</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="margin_bottom">
+        </div>
+
 </body>
+
 </html>
